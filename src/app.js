a7a37e93e03a0c0cfd29d6be18afc10bf2f3bda0
Irvin Do <irvindo@yahoo.com>
2018-02-12 02:19:05 -0500

    Finished

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const socketio = require('socket.io');
+const fs = require('fs');
+
+let score = 0;
+
+const handler = (req, res) => {
+  fs.readFile(`${__dirname}/../client/index.html`, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+};
+
+const app = http.createServer(handler);
+
+app.listen(3000);
+
+const io = socketio(app);
+
+io.on('connection', (socket) => {
+  socket.join('room1');
+
+  socket.on('updatePoints', (data) => {
+    score += data;
+    io.sockets.in('room1').emit('updated', score);
+  });
+
+  socket.on('disconnect', () => {
+    socket.leave('room1');
+  });
+});
